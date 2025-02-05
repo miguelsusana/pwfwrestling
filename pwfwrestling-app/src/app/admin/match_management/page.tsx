b928c1d084b66d4b0e1130eb4dd984273211cd14
Miguel Susana <miguelsusana1997@gmail.com>
2025-02-05 16:41:15 +0000

    Added new input boxes to ADD MATCH page for Match Participants

--- pwfwrestling-app/src/app/admin/match_management/page.tsx
+++ pwfwrestling-app/src/app/admin/match_management/page.tsx
@@ -25,12 +25,22 @@ type RosterData = {
     wrestler_id: number;
 }
 
+type MatchParticipantData = {
+    match_participant: string;
+    team_number: number | undefined;
+}
+
 export default function MatchManagement() {
 
     const [events, setEvents] = useState<EventData[]>([]);
     const [championships, setChampionships] = useState<ActiveChampionship[]>([]);
     const [isChampionship, setIsChampionship] = useState(false);
+    const [isCompetitor, setIsCompetitor] = useState(false);
     const [roster, setRoster] = useState<RosterData[]>([])
+    const [filteredRoster, setFilteredRoster] = useState<RosterData[]>([])
+    const [matchParticipants, setMatchParticipants] = useState<MatchParticipantData[]>([])
+    const [teamNumber, setTeamNumber] = useState<number | undefined>();
+    const [participants, setParticipants] = useState("");
 
     useEffect(() => {
         const fetchEvents = async () => {
@@ -57,12 +67,32 @@ export default function MatchManagement() {
         setIsChampionship(event.target.value === '1');
     };
 
+    const handleCompetitorStatusChange = (event: any) => {
+        setIsCompetitor(event.target.value === '1');
+    }
+
+    const handleWrestlerInputChange = (event: any) => {
+        const inputValue = event.target.value
+        setParticipants(inputValue)
+        if (inputValue.trim() === "") {
+            setFilteredRoster([]);
+        } else {
+            const filtered = roster.filter(wrestler => wrestler.wrestler_name.toLowerCase().startsWith(inputValue.toLowerCase()))
+            setFilteredRoster(filtered);
+        }
+    }
+
     const seasons = [...new Set(events.map(event => event.event_season))];
     const years = [...new Set(events.map(event => event.event_year))];
     const months = [...new Set(events.map(event => event.event_month))];
     const weeks = [...new Set(events.map(event => event.event_week))];
     const names = [...new Set(events.map(event => event.event_name))];
 
+    const addToParticipantList = () => {
+        if (participants && (teamNumber !== undefined && teamNumber > 0)) {
+            setMatchParticipants([...matchParticipants, { match_participant: participants, team_number: teamNumber }])
+        }
+    }
 
     return (
         <div className={styles.container}>
@@ -144,11 +174,34 @@ export default function MatchManagement() {
 
                         <div className={styles.form_group}>
                             <label htmlFor="name">Match Participants:</label>
-                            <input type="text" id="match_type" name="match_type" />
+                            <input type="text" id="match_participant" name="match_participant" onChange={handleWrestlerInputChange} />
+                            {filteredRoster.length > 0 && (
+                                <ul className={styles.sugg}>
+                                    {filteredRoster.map((wrestler, index) => (
+                                        <li className={styles.sugg2} key={index}>{wrestler.wrestler_name}</li>
+                                    ))}
+                                </ul>)}
+
+                            <label htmlFor="name">Team Number</label>
+                            <input type="number" id='team_number' name="team_number" onChange={(input) => setTeamNumber(input.target.valueAsNumber)} />
+                            <label>Is Competitor</label>
+                            <select id="is_competitor" onChange={handleCompetitorStatusChange}>
+                                <option value={1}>YES</option>
+                                <option value={0}>NO</option>
+                            </select>
+                            {isCompetitor && (
+                                <div>
+                                    <label htmlFor="name">Accompanied By</label>
+                                    <input type="text" id='accompanied_by' name="accompanied_by" />
+                                </div>)}
+                            <button type="button" onClick={addToParticipantList}>Add Superstar</button>
                         </div>
-
-
-
+                        {matchParticipants.map((p, index) => (
+                            <div key={index}>
+                                <div>{p.match_participant}</div>
+                                <div>{p.team_number}</div>
+                            </div >
+                        ))}
 
                         <div className={styles.filter_buttons}>
                             <button type="submit">Add Match</button>
